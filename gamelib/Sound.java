70987f2340b227f17d248d895bfa2daf146f5763
CodingLegend0100 <codinglegend0100@gmail.com>
2022-05-18 09:23:52 -0400

    Improved error logging

--- gamelib/Sound.java
+++ gamelib/Sound.java
@@ -4,15 +4,19 @@ import java.io.File;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 public class Sound {
     Clip clip;
 
     public Sound(String filepath) {
         try {
-        AudioInputStream ais = AudioSystem.getAudioInputStream(new File(filepath));
-        clip = AudioSystem.getClip();
-        clip.open(ais);
+            AudioInputStream ais = AudioSystem.getAudioInputStream(new File(filepath));
+            clip = AudioSystem.getClip();
+            clip.open(ais);
+        } catch (UnsupportedAudioFileException e){
+            System.out.println("The file type of "+filepath+" is unsupported!");
+            e.printStackTrace();
         } catch (Exception e){
             System.out.println("An error occurred while trying to play "+filepath);
             e.printStackTrace();
