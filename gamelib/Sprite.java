185cecefdb7fc82da9c79696d677a3a2d5d4b151
CodingLegend0100 <codinglegend0100@gmail.com>
2022-05-21 17:10:00 -0700

    Sprite improvements

--- gamelib/Sprite.java
+++ gamelib/Sprite.java
@@ -1,19 +1,23 @@
-package gamelib;
+package game;
 
 import java.awt.Image;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
+import java.util.ArrayList;
 import javax.swing.ImageIcon;
 
 //DO NOT EDIT THIS CLASS
+//Unless you know what you're doing
+//Then you may
 /** This class is intended for displaying static images onto the screen */
 public class Sprite {
     protected double x, y; //Represents the sprites CENTER x and y
+    protected double velX, velY, rotationSpeed;
     protected int width,height;
     protected double rotation; //Rotation in degrees
     protected Image i;
 
-    /** Blank constructor for a blank sprite */
+    /**Create a blank sprite */
     public Sprite(){}
 
     /**Create a sprite whose size will be inherited from the image's width and height */
@@ -66,19 +70,46 @@ public class Sprite {
     //Setters
     public void setX(double x){ this.x = x; }
     public void setY(double y){ this.y = y; }
-    public void setWdith(int width){ this.width = width; }
+    public void setPos(double x, double y){ this.x = x; this.y = y; }
+    public void setVelX(double x){ velX = x; }
+    public void setVelY(double y){ velY = y; }
+    public void setVelocity(double x, double y){ velX = x; velY = y; }
+    public void setWidth(int width){ this.width = width; }
     public void setHeight(int height){ this.height = height; }
     public void setRotation(double degrees){ this.rotation = degrees; }
+    public void setRotationSpeed(double degrees){ rotationSpeed = degrees; }
     public void setImage(Image i){ this.i = i; }
 
-    public boolean isTouching(Sprite s){
+    //Ignores rotation
+    public boolean checkCollision(Sprite s){
         //If this.right > s.left && this.left < s.right && this.bottom > s.top && this.top < s.bottom
-        Rectangle collide1 = new Rectangle((int)x+width/2,(int)y+height/2,width,height);
-        Rectangle collide2 = new Rectangle((int)s.getX()-s.getWidth()/2,(int)s.getY()-s.getHeight()/2,s.getWidth(),s.getHeight());
-        return collide1.intersects(collide2) || collide1.contains(collide2) || collide2.contains(collide1);
+        Rectangle r1 = new Rectangle((int)x-width/2,(int)y-height/2,width,height);
+        Rectangle r2 = new Rectangle((int)s.x-s.width/2,(int)s.y-s.height/2,s.width,s.height);
+        return r1.intersects(r2) || r1.contains(r2) || r2.contains(r1);
     }
 
-    public void update(){}
+    //Also ignores rotation
+    /** Checks if the specified point is is contained within the sprite */
+    public boolean contains(int x,int y){
+        return this.x < x && this.y < y && this.x+width > x && this.y+height > y;
+    }
+
+    /** Returns an ArrayList of sprites that this sprite is colliding with */
+    public ArrayList<Sprite> checkCollisionList(ArrayList<Sprite> sprites){
+        ArrayList<Sprite> colliding = new ArrayList<Sprite>();
+        for (Sprite s : sprites){
+            if (checkCollision(s)){
+                colliding.add(s);
+            }
+        }
+        return colliding;
+    }
+
+    public void update(){
+        x += velX;
+        y += velY;
+        rotation += rotationSpeed;
+    }
 
     /** Draws the sprite */
     public void draw(Graphics2D g){
